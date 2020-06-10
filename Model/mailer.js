2cf073a2f0217e9deb18d2593d7d850f9b1b1f4d
unknown <karenfahey@rocketmail.com>
2020-06-10 14:48:45 -0400

    minor edits

--- Model/mailer.js
+++ Model/mailer.js
@@ -1,26 +1,24 @@
-var nodemailer = require('nodemailer');
-
+var nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'akaglue2020@gmail.com',
-      pass: 'CHUKWUamaka2020!!'
-    }
-  });
-  
-  var mailOptions = {
-    from: 'akaglue2020@gmail.com',
-    to: 'karenfahey@rocketmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'Hi Karen, Trying out the nodemailer application! Amaka'
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-  
+  service: "gmail",
+  auth: {
+    user: "akaglue2020@gmail.com",
+    pass: "CHUKWUamaka2020!!",
+  },
+});
+
+var mailOptions = {
+  from: "akaglue2020@gmail.com",
+  to: "karenfahey@rocketmail.com",
+  subject: "Sending Email using Node.js",
+  text: "Hi Karen, Trying out the nodemailer application! Amaka",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
